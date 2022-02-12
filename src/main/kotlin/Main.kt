bd487f8f744acdb483bc47ce585247c1f5827f3c
BrendaMurugi <murugiibrenda@gmail.com>
2022-02-12 16:04:26 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,19 @@
+fun main() {
+    var name = "Brenda Murugi"
+    println(name)
+
+    var age = 21
+    println(age)
+
+    var number =  "0798881101"
+    println(number)
+
+    var weight: Float = 47.5F
+    println(weight)
+
+    val kenyan: Boolean = true
+    val notkenyan: Boolean = false
+    println(kenyan)   // Outputs true
+    println(notkenyan)   // Outputs false
+
+}
